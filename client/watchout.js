11fffc48bf5aeea4e55f04cfae79c447ae79d493
underaj <chingyang91@gmail.com>
2016-06-17 20:13:34 -0700

    collisions counter

--- client/watchout.js
+++ client/watchout.js
@@ -2,20 +2,24 @@ var WIDTH = 900;
 var HEIGHT = 500;
 var RADIUS = 20;
 var MOUSE_RADIUS = 10;
+var INITIAL = 3;
+var MAX = 20;
 var coordinates = [];
 var delay = 2000;
 var score = 0;
 var d3Score = d3.select('.current span').data([0]);
-var highScore = 0;
-var d3HighScore = d3.select('.highscore').select('span').data([0]);
-var drag = d3.behavior.drag()
-  .on('drag', function(d, i) {
-    d.x += d3.event.dx;
-    d.y += d3.event.dy;
-    d3.select(this).style('transform', function(d, i) {
-      return 'translate(' + d.x + 'px, ' + d.y + 'px)';
-    });
+var d3HighScore = d3.select('.highscore span').data([0]);
+var collisionCount = 0;
+var d3Collisions = d3.select('.collisions span').data([0]);
+var drag = d3.behavior.drag().on('drag', function(d, i) {
+  var newX = d.x + d3.event.dx;
+  var newY = d.y + d3.event.dy;
+  d.x = newX < 0 ? 0 : newX > WIDTH ? WIDTH : newX;
+  d.y = newY < 0 ? 0 : newY > HEIGHT ? HEIGHT : newY;
+  d3.select(this).style('transform', function(d, i) {
+    return 'translate(' + d.x + 'px, ' + d.y + 'px)';
   });
+});
 
 var svg = d3.select('.board')
   .append('svg')
@@ -27,7 +31,7 @@ svg.append('defs').append('pattern')
   .attr('width', 2 * RADIUS)
   .attr('height', 2 * RADIUS)
   .append('image')
-  .attr('xlink:href', 'asteroid.png')
+  .attr('xlink:href', 'shuriken.png')
   .attr('width', 2 * RADIUS)
   .attr('height', 2 * RADIUS);
 
@@ -39,6 +43,19 @@ mouse.call(drag);
 
 var randomUpTo = function(n) {
   return Math.floor(Math.random() * (n - 2 * RADIUS)) + RADIUS;
+};
+
+var randomCoord = function() {
+  return {
+    cx: randomUpTo(WIDTH),
+    cy: randomUpTo(HEIGHT)
+  };
+};
+
+var makeInitialData = function() {
+  for (var i = 0; i < INITIAL; i++) {
+    coordinates.push(randomCoord());
+  }
 };
 
 var update = function() {
@@ -59,7 +76,15 @@ var update = function() {
     .attr('cx', function(d) { return d.cx; })
     .attr('cy', function(d) { return d.cy; })
     .attr('r', RADIUS)
-    .attr('fill', 'url("#asteroid")');
+    .attr('fill', 'url("#asteroid")')
+    .classed('asteroid', true);
+  circle
+    .exit()
+    .remove();
+
+  if (coordinates.length < MAX) {
+    coordinates.push(randomCoord());
+  }
 };
 
 var tick = function() {
@@ -83,6 +108,7 @@ var collisionCheck = function() {
     var cy = d3.select(this).attr('cy');
     if (collision(mP, {cx: cx, cy: cy})) {
       hasCollision = true;
+      collisionCount++;
     }
   });
   if (hasCollision) {
@@ -90,6 +116,12 @@ var collisionCheck = function() {
       d3HighScore.data([score])
         .text(function(d) { return d; });
     }
+    d3Collisions.data([collisionCount]).text(function(d) { return d; });
+    coordinates.length = 0;
+    update();
+    makeInitialData();
+    update();
+    update();
     score = 0;
   }
   
@@ -97,10 +129,7 @@ var collisionCheck = function() {
     .text(function(d) { return d; });
 };
 
-for (var i = 0; i < 3; i++) {
-  coordinates.push({});
-}
-
+makeInitialData();
+update();
 tick();
-
 setInterval(collisionCheck, 100);
