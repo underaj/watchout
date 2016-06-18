ff423756fc8c2ca5494f30f1da3f6b1498f81284
underaj <chingyang91@gmail.com>
2016-06-17 17:30:59 -0700

    Implement score board

--- client/watchout.js
+++ client/watchout.js
@@ -1,8 +1,13 @@
 var WIDTH = 900;
 var HEIGHT = 500;
 var RADIUS = 20;
+var MOUSE_RADIUS = 10;
 var coordinates = [];
-var delay = 1000;
+var delay = 2000;
+var score = 0;
+var d3Score = d3.select('.current span').data([0]);
+var highScore = 0;
+var d3HighScore = d3.select('.highscore').select('span').data([0]);
 var drag = d3.behavior.drag()
   .on('drag', function(d, i) {
     d.x += d3.event.dx;
@@ -10,13 +15,13 @@ var drag = d3.behavior.drag()
     d3.select(this).style('transform', function(d, i) {
       return 'translate(' + d.x + 'px, ' + d.y + 'px)';
     });
-  }); 
+  });
 
 var svg = d3.select('.board')
   .append('svg')
   .attr('width', WIDTH)
   .attr('height', HEIGHT);
-  
+
 svg.append('defs').append('pattern')
   .attr('id', 'asteroid')
   .attr('width', 2 * RADIUS)
@@ -27,7 +32,9 @@ svg.append('defs').append('pattern')
   .attr('height', 2 * RADIUS);
 
 var mouse = d3.select('.mouse');
-mouse.data([{x: 0, y: 0}]);
+mouse.data([{x: 0, y: 0}])
+  .style('width', 2 * MOUSE_RADIUS + 'px')
+  .style('height', 2 * MOUSE_RADIUS + 'px');
 mouse.call(drag);
 
 var randomUpTo = function(n) {
@@ -43,7 +50,7 @@ var update = function() {
   var circle = svg.selectAll('circle')
     .data(coordinates);
   circle
-    .transition(delay)
+    .transition().delay(delay)
     .attr('cx', function(d) { return d.cx; })
     .attr('cy', function(d) { return d.cy; });
   circle  
@@ -60,8 +67,31 @@ var tick = function() {
   setTimeout(tick, delay);
 };
 
+var collision = function(m, c) {
+  return Math.sqrt(Math.pow(m.x + MOUSE_RADIUS - c.cx, 2) + 
+    Math.pow(m.y - MOUSE_RADIUS - c.cy, 2)) <= MOUSE_RADIUS + RADIUS; 
+};
+
+var collisionCheck = function() {
+  var mP = mouse.datum();
+  var cP = svg.selectAll('circle').data();
+  // Some stop iterating when a true value is returned from the callback function.
+  if (cP.some(collision.bind(null, mP))) {
+    if (d3HighScore.datum() < score) {
+      d3HighScore.data([score])
+        .text(function(d) { return d; });
+    }
+    score = 0;
+  }
+  
+  d3Score.data([++score])
+    .text(function(d) { return d; });
+};
+
 for (var i = 0; i < 3; i++) {
   coordinates.push({});
 }
 
 tick();
+
+setInterval(collisionCheck, 50);
